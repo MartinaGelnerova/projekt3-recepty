173c4fd6553175fc31faf9550e26fd206d6fbf1a
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-29 11:24:14 +0200

    seznam receptů

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
+//1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
+//HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.
+let element = null;
+
+seznamReceptu(recepty, element);
+
+function seznamReceptu(recepty, element) {
+  for (i = 0; i <= recepty.length - 1; i++) {
+    element = recepty[i];
+    let recept = document.createElement('div');
+    recept.className = 'recept';
+    recept.setAttribute("recept-cislo", i);
+    document.getElementById('recepty').appendChild(recept);
+    let receptObrazek = document.createElement('div');
+    receptObrazek.className = 'recept-obrazek';
+    receptObrazek.setAttribute("obrazek-cislo", i);
+    document.querySelector(`[recept-cislo="${i}"]`).appendChild(receptObrazek);
+    let receptImg = document.createElement('img');
+    receptImg.alt = "Obrazek";
+    receptImg.src = element.img;
+    document.querySelector(`[obrazek-cislo="${i}"]`).appendChild(receptImg);
+    let receptInfo = document.createElement('div');
+    receptInfo.className = 'recept-info';
+    receptInfo.setAttribute("recept-info", i);
+    document.querySelector(`[recept-cislo="${i}"]`).appendChild(receptInfo);
+    let receptTitle = document.createElement('h3');
+    receptTitle.innerText = element.nadpis;
+    document.querySelector(`[recept-info="${i}"]`).appendChild(receptTitle);
+  }
+}
+
 /*
 Co je za úkol v tomto projektu:
 
-1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
-HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.
 
 2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
 by se měl seznam receptů vyfiltrovat podle hledaného slova.
