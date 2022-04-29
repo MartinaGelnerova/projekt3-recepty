e83347654cf63f98ecc7bf0ee43d39ac96b00859
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-29 16:06:47 +0200

    fitltrace podle kategorie

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
-//1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
-//HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.
 let element = null;
+let receptyFiltr = [];
+let seznam = [];
 
-seznamReceptu(recepty, element);
+//1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
+seznamReceptu();
 
-function seznamReceptu(recepty, element) {
-  for (i = 0; i <= recepty.length - 1; i++) {
-    element = recepty[i];
+//2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
+//by se měl seznam receptů vyfiltrovat podle hledaného slova.
+
+
+//3) Doplň filtrovanání receptů podle kategorie.
+let kategorie = document.getElementById('kategorie');
+kategorie.addEventListener('change', katFiltrace);
+
+
+
+
+
+/*
+
+4) Doplň řazení receptů podle hodnocení.
+
+5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
+Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
+recept-hodnoceni, recept-nazev, recept-popis.
+
+6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
+*/
+
+// FUNKCE
+function seznamReceptu() {
+  seznam = [];
+  if (receptyFiltr.length == 0) {
+    for (i=0; i < recepty.length; i++) {
+      seznam.push(recepty[i])
+    }
+  } else {
+    for (i=0; i < receptyFiltr.length; i++) {
+      seznam.push(recepty[receptyFiltr[i]])
+    }
+  }
+  document.querySelector('.kontejner').removeChild(document.getElementById('recepty'));
+  let receptyDiv = document.createElement('div');
+  receptyDiv.id = 'recepty';
+  receptyDiv.className = 'recepty';
+  document.querySelector('.kontejner').appendChild(receptyDiv);
+  for (i = 0; i < seznam.length; i++) {
+    element = seznam[i];
     let recept = document.createElement('div');
     recept.className = 'recept';
     recept.setAttribute("recept-cislo", i);
@@ -25,24 +65,36 @@ function seznamReceptu(recepty, element) {
     document.querySelector(`[recept-cislo="${i}"]`).appendChild(receptInfo);
     let receptTitle = document.createElement('h3');
     receptTitle.innerText = element.nadpis;
-    document.querySelector(`[recept-info="${i}"]`).appendChild(receptTitle);
+    document.querySelector(`[recept-info="${i}"]`).appendChild(receptTitle)
   }
 }
 
-/*
-Co je za úkol v tomto projektu:
+function kategorieValue() {
+  kategorie = document.getElementById('kategorie');
+  let katValue = kategorie.value;
+  return (katValue);
+}
 
+function katFiltrace() {
+  katValue = kategorieValue();
+  console.log(katValue);
+  receptyFiltr = [];
+  for (n = 0; n <= recepty.length - 1; n++) {
+    if (katValue == '') {
+      receptyFiltr.push(n);
+    } else if (recepty[n].kategorie === katValue) {
+      receptyFiltr.push(n);
+    }
+  }
+  console.log(receptyFiltr);
+  seznamFiltrovany()
+}
 
-2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
-by se měl seznam receptů vyfiltrovat podle hledaného slova.
-
-3) Doplň filtrovanání receptů podle kategorie.
-
-4) Doplň řazení receptů podle hodnocení.
-
-5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
-Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
-recept-hodnoceni, recept-nazev, recept-popis.
-
-6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
-*/
+function seznamFiltrovany() {
+  seznam = [];
+  for (let i=0; i<receptyFiltr.length; i=i+1) {
+    seznam.push(recepty[receptyFiltr[i]]);
+    console.log(seznam);
+    seznamReceptu()
+  }
+}
